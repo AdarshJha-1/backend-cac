7395b9e4f8298dfbeb42ff9fa44bcf0b7e07235c
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-05 12:50:46 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-export const app = express();
+const app = express();
 
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    Credential: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser())
+
+export { app };
